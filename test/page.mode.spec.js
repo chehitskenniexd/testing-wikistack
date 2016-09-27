d4edfa68eab740f12c8f4f9044c405409e53ea96
Kenneth Moy <moykenneth91@gmail.com>
2016-09-27 13:05:26 -0400

    Wrote an outline for tests in page model

---
+++ test/page.mode.spec.js
@@ -0,0 +1,41 @@
+var models = require('../models');
+
+describe('The Models', () => {
+    describe('A Page:', () => {
+        it('Has a title', () => {
+
+        }); // end it ''
+        it('Has a valid urlTitle', () => {
+
+        }); // end it ''
+        it('Has some content', () => {
+
+        }); // end it ''
+        it('Has a status of either open or closed', () => {
+
+        }); // end it ''
+        it('Has a hook to validate urlTitle', () => {
+
+        }); // end it ''
+        it('Has getterMethods for a valid route and marked content', () => {
+
+        }); // end it ''
+        it('Has a classMethod to find pages by tags', () => {
+
+        }); // end it ''
+        it('Has an instanceMethod to find similar pages by tag', () => {
+
+        }); // end it ''
+        it('Has an author', () => {
+
+        }); // end it ''
+    }); // end describe 'The Page Model'
+    describe('A User:', () => {
+        it('Has a name', () => {
+
+        }); // end it ''
+        it('Has a valid email', () => {
+
+        }); // end it ''
+    }); // end describe 'The User Model'
+}); // end describe 'The Models'
